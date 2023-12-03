3e4b452c0f49316c52153d0e1d84ed2d2a7db343
ian <cutaksutarman@gmail.com>
2023-12-03 23:35:42 +0700

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,7 +152,7 @@ export default function Home() {
         {/* End Project Section */}
 
         {/* Experience Section */}
-        <div className="mt-[100px] ml-3">
+        <div className="mt-[100px] ml-3 md:hidden">
           <h1 className="text-3xl font-bold mb-10 -ml-3">
             🧑‍💻 <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#11C5C6] to-[#26C9FC]">Experiences</span>
           </h1>
@@ -185,6 +185,73 @@ export default function Home() {
           </ol>
         </div>
 
+        {/* if screen md--- */}
+        <div className="relative hidden md:block">
+          <h1 className="text-3xl font-bold mb-10 -ml-3 mt-10">
+            🧑‍💻 <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#11C5C6] to-[#26C9FC]">Experiences</span>
+          </h1>
+          <div className="contains ">
+            <ol className=" relative border-s border-gray-200 dark:border-gray-700">
+              {experienceItem.map((item, index) => {
+                if (index < 2) {
+                  return (
+                    <li key={index} className="mb-10 ms-6">
+                      <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                        <img className="rounded-full shadow-lg" src="/img/logo.png" alt="Thomas Lean image" />
+                      </span>
+                      <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+                        <span className="hidden md:inline text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+                        <div className="items-center justify-between mb-3 sm:flex">
+                          <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{item.year}</time>
+                          <br className="md:hidden" />
+                          <span className="md:hidden text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+                          <div className="mt-1 text-sm font-normal text-gray-500 lex dark:text-gray-300">
+                            {item.title}{" "}
+                            <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">
+                              {item.companyName}
+                            </a>
+                          </div>
+                        </div>
+                        <div className="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">{item.desc}</div>
+                      </div>
+                    </li>
+                  );
+                }
+                return null;
+              })}
+            </ol>
+            <ol className=" relative border-s border-gray-200 dark:border-gray-700">
+              {experienceItem.map((item, index) => {
+                if (index >= 2) {
+                  return (
+                    <li key={index} className="mb-10 ms-6">
+                      <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                        <img className="rounded-full shadow-lg" src="/img/logo.png" alt="Thomas Lean image" />
+                      </span>
+                      <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+                        <span className="hidden md:inline text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+                        <div className="items-center justify-between mb-3 sm:flex">
+                          <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{item.year}</time>
+                          <br className="md:hidden" />
+                          <span className="md:hidden text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+                          <div className="mt-1 text-sm font-normal text-gray-500 lex dark:text-gray-300">
+                            {item.title}{" "}
+                            <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">
+                              {item.companyName}
+                            </a>
+                          </div>
+                        </div>
+                        <div className="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">{item.desc}</div>
+                      </div>
+                    </li>
+                  );
+                }
+                return null; // Return null for items not meeting the condition
+              })}
+            </ol>
+          </div>
+          <div className="absolute bottom-0 w-[100%] h-[60px] bg-gradient-to-t from-[azure] blur-sm"></div>
+        </div>
         {/* End Experience Section */}
       </main>
 
