bf3344d04b7fb233bfde7cd29f89e09302d81493
ian <cutaksutarman@gmail.com>
2023-12-03 16:33:49 +0700

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -160,14 +160,16 @@ export default function Home() {
             {experienceItem.map((item, index) => {
               return (
                 <li key={index} className="mb-10 ms-6">
-                  <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <span className=" absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                     <img className="rounded-full shadow-lg" src="/img/logo.png" alt="Thomas Lean image" />
                   </span>
                   <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+                    <span className="hidden md:inline text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
                     <div className="items-center justify-between mb-3 sm:flex">
                       <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{item.year}</time>
-                      <br />
-                      <span className="text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+                      <br className="md:hidden" />
+                      <span className="md:hidden  text-[12px] text-[#11cde5] border border-[#11cde5] px-2 py-1 rounded-md ">{item.status}</span>
+
                       <div className="mt-1 text-sm font-normal text-gray-500 lex dark:text-gray-300">
                         {item.title}{" "}
                         <a href="#" className="font-semibold text-gray-900 dark:text-white hover:underline">
